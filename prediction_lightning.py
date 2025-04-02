2315165d5922d952d5cbd94091d7a031918c1281
Ting835 <69649229+RainyRaven@users.noreply.github.com>
2025-04-02 11:54:56 -0700

    after editing the model still failed to predict the price

--- prediction_lightning.py
+++ prediction_lightning.py
@@ -15,25 +15,21 @@ print(df.head())
 
 ##building dict for verbal data
 no_rep_cut = df["cut"].drop_duplicates()  # retrive none duplicated items
+v_cut = [4, 3, 1, 2, 0]
 v2c_cut = {
-    verbal: i for i, verbal in enumerate(no_rep_cut)
+    c: v for c, v in zip(no_rep_cut, v_cut)
 }  # create verbal to code dictionary for column "cut"
 print(v2c_cut)
 no_rep_color = df["color"].drop_duplicates()  # retrive none duplicated items
+v_color = [5, 1, 0, 2, 4, 3, 6]
 v2c_color = {
-    verbal: i for i, verbal in enumerate(no_rep_color)
+    c: v for c, v in zip(no_rep_color, v_color)
 }  # create verbal to code dictionary for column "color"
 print(v2c_color)
 no_rep_clarity = df["clarity"].drop_duplicates()  # retrive none duplicated items
-v2c_clarity = {
-    verbal: i for i, verbal in enumerate(no_rep_clarity)
-}  # create verbal to code dictionary for column "clarity"
+v_clarity = [1, 2, 4, 3, 5, 6, 0, 7]
+v2c_clarity = {c: v for c, v in zip(no_rep_clarity, v_clarity)}
 print(v2c_clarity)
-
-#####temp data for faster loading
-directory = "data\diamonds\diamonds_100.csv"  # can use url
-df = pd.read_csv(directory, delimiter=",")
-print(df.head())
 
 ##substitute verbal with codes
 df["cut"] = df["cut"].map(v2c_cut)  # map/replace verbal with int
@@ -48,12 +44,15 @@ label = torch.FloatTensor(y.values)
 
 # train test split
 train_input, test_input, train_label, test_label = train_test_split(
-    input, label, test_size=0.2, random_state=81
+    input,
+    label,
+    test_size=0.1,
+    random_state=81,  # 9:1 split bc the sheer size of the dataset
 )
 train_dataset = TensorDataset(train_input, train_label)
 test_dataset = TensorDataset(test_input, test_label)
 # dataloader
-batch_size = 64  # train faster
+batch_size = 256  # train faster
 train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
 test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
 # endregion
@@ -61,16 +60,20 @@ test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
 
 # region model
 class L_prediction(L.LightningModule):
-    def __init__(self, in_feature=9, h1=18, h2=19, out_feature=1):
+    def __init__(self, in_feature=9, h1=18, h2=9, h3=5, h4=3, out_feature=1):
         super().__init__()
         self.fc1 = nn.Linear(in_feature, h1)
         self.fc2 = nn.Linear(h1, h2)
-        self.out = nn.Linear(h2, out_feature)
-        self.learning_rate = 0.01
+        self.fc3 = nn.Linear(h2, h3)
+        self.fc4 = nn.Linear(h3, h4)
+        self.out = nn.Linear(h4, out_feature)
+        self.learning_rate = 0.1
 
     def forward(self, input):
         x = F.relu(self.fc1(input))
         x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.relu(self.fc4(x))
         x = self.out(x)
         return x
 
@@ -85,14 +88,18 @@ class L_prediction(L.LightningModule):
     def training_step(self, batch, batch_idx):
         input_i, label_i = batch
         output_i = self.forward(input_i)
-        loss = F.mse_loss(output_i, label_i)  # must use loss functions
+        criterion = nn.MSELoss()  ##separate lines to be more adaptive
+        loss = criterion(output_i, label_i)
         self.log("train_loss", loss)  # logging loss
+        accuracy = self.calculate_accuracy(output_i, label_i)
+        self.log("train_accuracy", accuracy)  # logging accuracy
         return loss
 
     def test_step(self, batch, batch_idx):
         input_i, label_i = batch
         output_i = self.forward(input_i)
-        loss = F.mse_loss(output_i, label_i)
+        criterion = nn.MSELoss()  ##separate lines to be more adaptive
+        loss = criterion(output_i, label_i)
         return loss
 
 
@@ -102,14 +109,14 @@ class L_prediction(L.LightningModule):
 # train_dataloader
 torch.manual_seed = 81
 model = L_prediction()
-epochs = 300
+epochs = 100
 trainer = L.Trainer(max_epochs=epochs, log_every_n_steps=10)
 # learning rate finder
 tuner = L.pytorch.tuner.Tuner(trainer)
 lr_find_results = tuner.lr_find(
     model,
     train_dataloaders=train_loader,
-    min_lr=0.01,
+    min_lr=0.1,
     max_lr=1.0,
     early_stop_threshold=None,
 )
@@ -118,6 +125,11 @@ model.learning_rate = new_lr
 print(f"lr_find() suggest {new_lr:5f} for the learning rate.")
 # train to fit
 trainer.fit(model, train_dataloaders=train_loader)
+
+# continue training
+path_to_best_checkpoint = trainer.checkpoint_callback.best_model_path
+trainer = L.Trainer(max_epochs=300, log_every_n_steps=2)
+trainer.fit(model, train_dataloaders=train_loader, ckpt_path=path_to_best_checkpoint)
 
 # evaluating train/loss result
 # use tensorboard in terminal
@@ -133,40 +145,20 @@ with torch.no_grad():
     correct = 0
     for i, data in enumerate(test_input):
         test_res = model.forward(test_input).detach()
-        test_res_arg = torch.argmax(test_res, dim=1, keepdim=False)
-        print(f"{i + 1}.) {test_res[i]} \t {test_res_arg[i]} \t {test_label[i]}")
-        if test_res_arg[i] == test_label[i]:
+        print(f"{i + 1}.) {test_res[i]} \t {test_label[i]}")
+        if test_res[i] == test_label[i]:
             correct += 1
         accuracy = model.calculate_accuracy(test_res, test_label)
     print(f"{correct} / {len(test_label)} are correct")
     print(f"accuracy = {accuracy * 100}%")
 
 # saving model and parameters
-torch.save(model.state_dict(), "model\l_id_iris.pt")
+torch.save(model.state_dict(), "model\l_pd_diamonds.pt")
 
 # endregion
 
 
-# region Identify by model
+# Identify by model
 # load model and parameters
 loaded_model = L_prediction()
-loaded_model.load_state_dict(torch.load("model\l_id_iris.pt"))
-
-# identify
-with torch.no_grad():
-    sus_input = torch.tensor(
-        [[5, 3.5, 1.6, 0.6], [5.1, 2.5, 3, 1.1], [6.9, 3.2, 5.7, 2.3]]
-    )
-    print("the iris types are:")
-    for i, data in enumerate(sus_input):
-        sus_res = loaded_model.forward(sus_input).clone().detach()
-        sus_res_arg = torch.argmax(sus_res, dim=1, keepdim=False)
-        if sus_res_arg[i] == 0:
-            sus = "Setosa"
-        elif sus_res_arg[i] == 1:
-            sus = "Versicolor"
-        else:
-            sus = "Virginica"
-        print(f"{i + 1}.) {sus}")
-
-# endregion
+loaded_model.load_state_dict(torch.load("model\l_pd_diamonds.pt"))
