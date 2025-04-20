0f50bb4d5fb7ee3c4c3ebbe8fef6031793397805
Ting835 <69649229+RainyRaven@users.noreply.github.com>
2025-04-19 17:04:37 -0700

    normalized data yet model accuracy still at 0

--- Boston_house_prediction_lightning .py
+++ Boston_house_prediction_lightning .py
@@ -2,42 +2,27 @@ import torch
 import torch.nn as nn
 import lightning as L
 import torch.nn.functional as F
-from torch.optim import Adam
+from torch.optim import SGD
 from torch.utils.data import TensorDataset, DataLoader
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
+import numpy as np
 
 # region dataset
 # import and modify data with pandas
-directory = "data\diamonds\diamonds.csv"  # can use url
+directory = "data\Boston\BostonHousing.csv"  # can use url
 df = pd.read_csv(directory, delimiter=",")
 print(df.head())
 
-##building dict for verbal data
-no_rep_cut = df["cut"].drop_duplicates()  # retrive none duplicated items
-v_cut = [4, 3, 1, 2, 0]
-v2c_cut = {
-    c: v for c, v in zip(no_rep_cut, v_cut)
-}  # create verbal to code dictionary for column "cut"
-print(v2c_cut)
-no_rep_color = df["color"].drop_duplicates()  # retrive none duplicated items
-v_color = [5, 1, 0, 2, 4, 3, 6]
-v2c_color = {
-    c: v for c, v in zip(no_rep_color, v_color)
-}  # create verbal to code dictionary for column "color"
-print(v2c_color)
-no_rep_clarity = df["clarity"].drop_duplicates()  # retrive none duplicated items
-v_clarity = [1, 2, 4, 3, 5, 6, 0, 7]
-v2c_clarity = {c: v for c, v in zip(no_rep_clarity, v_clarity)}
-print(v2c_clarity)
-
-##substitute verbal with codes
-df["cut"] = df["cut"].map(v2c_cut)  # map/replace verbal with int
-df["color"] = df["color"].map(v2c_color)
-df["clarity"] = df["clarity"].map(v2c_clarity)
+# normalization with MinMaxscaler
+scaler = MinMaxScaler()
+df.iloc[:, np.r_[1:3, 5:14]] = scaler.fit_transform(df.iloc[:, np.r_[1:3, 5:14]])
+##normalize all column except number 4 "CHAS"
 print(df.head())
-X = df.drop(["price"], axis=1)  # axis=1 =remove column, axis=0 = remove 1st row/index
-y = df["price"]
+
+X = df.drop(["medv"], axis=1)  # axis=1 =remove column, axis=0 = remove 1st row/index
+y = df["medv"]
 # assigning inputs and labels and convert to tensor
 input = torch.FloatTensor(X.values)
 label = torch.FloatTensor(y.values)
@@ -46,13 +31,13 @@ label = torch.FloatTensor(y.values)
 train_input, test_input, train_label, test_label = train_test_split(
     input,
     label,
-    test_size=0.1,
-    random_state=81,  # 9:1 split bc the sheer size of the dataset
+    test_size=0.2,
+    random_state=81,
 )
 train_dataset = TensorDataset(train_input, train_label)
 test_dataset = TensorDataset(test_input, test_label)
 # dataloader
-batch_size = 256  # train faster
+batch_size = 32
 train_loader = DataLoader(train_dataset, shuffle=True, batch_size=batch_size)
 test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
 # endregion
@@ -60,25 +45,23 @@ test_loader = DataLoader(test_dataset, shuffle=False, batch_size=batch_size)
 
 # region model
 class L_prediction(L.LightningModule):
-    def __init__(self, in_feature=9, h1=18, h2=9, h3=5, h4=3, out_feature=1):
+    def __init__(self, in_feature=13, h1=20, h2=13, h3=13, out_feature=1):
         super().__init__()
         self.fc1 = nn.Linear(in_feature, h1)
         self.fc2 = nn.Linear(h1, h2)
         self.fc3 = nn.Linear(h2, h3)
-        self.fc4 = nn.Linear(h3, h4)
-        self.out = nn.Linear(h4, out_feature)
-        self.learning_rate = 0.1
+        self.out = nn.Linear(h3, out_feature)
+        self.learning_rate = 0.0001
 
     def forward(self, input):
         x = F.relu(self.fc1(input))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
         x = self.out(x)
         return x
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=self.learning_rate)
+        return SGD(self.parameters(), lr=self.learning_rate)
 
     def calculate_accuracy(self, output, label):
         total = label.size(0)
@@ -109,14 +92,15 @@ class L_prediction(L.LightningModule):
 # train_dataloader
 torch.manual_seed = 81
 model = L_prediction()
-epochs = 100
+epochs = 300
 trainer = L.Trainer(max_epochs=epochs, log_every_n_steps=10)
+
 # learning rate finder
 tuner = L.pytorch.tuner.Tuner(trainer)
 lr_find_results = tuner.lr_find(
     model,
     train_dataloaders=train_loader,
-    min_lr=0.1,
+    min_lr=0.0001,
     max_lr=1.0,
     early_stop_threshold=None,
 )
@@ -128,7 +112,7 @@ trainer.fit(model, train_dataloaders=train_loader)
 
 # continue training
 path_to_best_checkpoint = trainer.checkpoint_callback.best_model_path
-trainer = L.Trainer(max_epochs=300, log_every_n_steps=2)
+trainer = L.Trainer(max_epochs=500, log_every_n_steps=2)
 trainer.fit(model, train_dataloaders=train_loader, ckpt_path=path_to_best_checkpoint)
 
 # evaluating train/loss result
@@ -153,7 +137,7 @@ with torch.no_grad():
     print(f"accuracy = {accuracy * 100}%")
 
 # saving model and parameters
-torch.save(model.state_dict(), "model\l_pd_diamonds.pt")
+torch.save(model.state_dict(), "model\l_pd_BostonHousing.pt")
 
 # endregion
 
--- README.md
+++ README.md
@@ -2,10 +2,14 @@ Purpose of the repository:
 This repository is an exhibition of the machine learning models I have programed using popular datasets. Despite being a junior level self taught programmer, the models shown here did have readability, maintainability and scalability in mind when programmed, so that incase of need they can be quickly fitted on other datasets; or used by others thereof. Still I am aware that my programming skills are far from perfect, so please feel free to give me pointers :)
 
 Structure:
-data : raw data files are stored
+data : raw data files (.csv) are stored
 model: trained parameters of various models are stored
 python: .py files are stored, each has matching name with their jupyter notebook version.
 
 Current trained models includes:
 
-    1. identification_lightning.ipynb: Pytorch lightning nn for identification (using iris dataset (1988))
+    1. Iris_identification_lightning.ipynb: Pytorch lightning nn for identification (using iris dataset (1988))
+
+
+Failed model
+Prediction model for Diamonds dataset, failed possiblly bc there are 3 categorical columns which did not provide sufficient learning material to predict a price variation between $326~18823, despite the sheer size of the dataset.
